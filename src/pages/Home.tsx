556a1db875a7873430b084d4cca3918c0f161ffa
BahaaEldin0 <b.moustafa@nu.edu.eg>
2023-08-13 19:01:10 +0300

    edits on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,11 @@ import Footer from '../components/Footer';
 import AppleIcon from '@mui/icons-material/Apple';
 import GooglePlayIcon from '@mui/icons-material/PlayCircleOutline';
 import HuaweiIcon from '../assets/images/HuaweiIcon.jsx';
+
+
 import redPhones3 from '../assets/images/3 red phones.png';
+import allCars from '../assets/images/all vehicles.png'
+import allCarTypes from '../assets/images/vehicle types.png'
 const theme = createTheme();
 
 
@@ -64,6 +68,7 @@ const Home: React.FC = () => {
 
       <Container>
 
+      {/* Download Buttons and Intro */}
       <StyledSection>
       <Grid container spacing={2} alignItems="center" justifyContent="center">
         <Grid item xs={12} sm={6}>
@@ -107,87 +112,159 @@ const Home: React.FC = () => {
     </StyledSection>
 
 
-        <StyledSection>
-          <Grid container spacing={2} alignItems="center" justifyContent="center">
-            
-            <Grid item xs={12} sm={6}>
-              <TextColumn>
-                <Typography variant="h5">Header Title 1</Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque nulla
-                  eget urna consequat, ut congue arcu facilisis.
-                </Typography>
-              </TextColumn>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <ImageColumn>
-                <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
-              </ImageColumn>
-            </Grid>
-          </Grid>
-        </StyledSection>
+    {/* Why Spark Ride with 3 cars and heli*/}
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center" sx={{mt:20}}>
+        <Grid item xs={12} sm={12}>
+          <ImageColumn>
+            <img src={allCars} alt="Image 1" style={{ maxWidth: '140%' }} />
+          </ImageColumn>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <TextColumn>
+            <Typography variant="h5" sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }} >All kinds of rides</Typography>
+            <Typography sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }}>
+            With us, you can choose from a wide range of car types.
+            </Typography>
+          </TextColumn>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <TextColumn>
+          <Typography variant="h5" sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }}>Spend less money</Typography>
+            <Typography sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }}>
+            Every city we serve offers competitive prices. See for yourself!
+            </Typography>
+          </TextColumn>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <TextColumn>
+          <Typography variant="h5" sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }}>Your ride your way</Typography>
+            <Typography sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }}>
+            Pick your destination, request a ride, meet your driver, enjoy the journey
+            </Typography>
+          </TextColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
 
 
-        <StyledSection>
-          <Grid container spacing={2} alignItems="center" justifyContent="center">
-            <Grid item xs={12} sm={6}>
-              <ImageColumn>
-                <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
-              </ImageColumn>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextColumn>
-                <Typography variant="h5">Header Title 1</Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque nulla
-                  eget urna consequat, ut congue arcu facilisis.
-                </Typography>
-              </TextColumn>
-            </Grid>
-          </Grid>
-        </StyledSection>
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center" sx={{mt:20}}>
+
+        <Grid item xs={12} sm={6}>
+          <TextColumn>
+            <Typography variant="h4"><span style={{fontWeight:'bold'}}>Better </span>Ways To Get Around</Typography>
+            <Typography>
+            You can count on us for all your transportation needs. We can transport your luggage & items easily, whether you need a ride or a service. Take advantage of hassle-free transportation by downloading the Spark Ride app today.
+            </Typography>
+          </TextColumn>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <ImageColumn>
+            <img src={allCarTypes} alt="Image 1" style={{ maxWidth: '100%' }} />
+          </ImageColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
+
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={6}>
+          <ImageColumn>
+            <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
+          </ImageColumn>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextColumn>
+            <Typography variant="h5">SOS Button & Safety</Typography>
+            <Typography>
+            SOS button to keep you safe any time. You can send Your ride details and live location to your friends and family.
+            </Typography>
+          </TextColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
+
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
+        
+        <Grid item xs={12} sm={6}>
+          <TextColumn>
+            <Typography variant="h5">Pay by Card, Wallet & Cash</Typography>
+            <Typography>
+            You can pay in the way you like whether by cash or your wallet balance or by credit card you will always find your way.
+            </Typography>
+          </TextColumn>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <ImageColumn>
+            <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
+          </ImageColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
+
+
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={6}>
+          <ImageColumn>
+            <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
+          </ImageColumn>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextColumn>
+            <Typography variant="h5">Favourite Addresses</Typography>
+            <Typography>
+              You can save multiple addresses to save time for all your repeating rides.
+            </Typography>
+          </TextColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
 
 
 
-        <StyledSection>
-          <Grid container spacing={2} alignItems="center" justifyContent="center">
-            
-            <Grid item xs={12} sm={6}>
-              <TextColumn>
-                <Typography variant="h5">Header Title 1</Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque nulla
-                  eget urna consequat, ut congue arcu facilisis.
-                </Typography>
-              </TextColumn>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <ImageColumn>
-                <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
-              </ImageColumn>
-            </Grid>
-          </Grid>
-        </StyledSection>
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
+        
+        <Grid item xs={12} sm={6}>
+          <TextColumn>
+            <Typography variant="h5">Coupons & Referral Programs</Typography>
+            <Typography>
+            Invite users and gain free credit, discounts, and special offers.Keep following to get new deals and offers on your rides.
+            </Typography>
+          </TextColumn>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <ImageColumn>
+            <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
+          </ImageColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
 
 
-        <StyledSection>
-          <Grid container spacing={2} alignItems="center" justifyContent="center">
-            <Grid item xs={12} sm={6}>
-              <ImageColumn>
-                <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
-              </ImageColumn>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextColumn>
-                <Typography variant="h5">Header Title 1</Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque nulla
-                  eget urna consequat, ut congue arcu facilisis.
-                </Typography>
-              </TextColumn>
-            </Grid>
-          </Grid>
-        </StyledSection>
+    <StyledSection>
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={6}>
+          <ImageColumn>
+            <img src={redPhones3} alt="Image 1" style={{ maxWidth: '100%' }} />
+          </ImageColumn>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextColumn>
+            <Typography variant="h5">Favourite Addresses</Typography>
+            <Typography>
+              You can save multiple addresses to save time for all your repeating rides.
+            </Typography>
+          </TextColumn>
+        </Grid>
+      </Grid>
+    </StyledSection>
         
       </Container>
 
